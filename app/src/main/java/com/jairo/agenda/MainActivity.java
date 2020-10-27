b00a56d6494a7d3fa8cd8b86fcbaea52df942a42
jairo <agust_sani@hotmail.com>
2020-10-26 21:17:39 -0600

    Se envían eventos a Firebase

--- app/src/main/java/com/jairo/agenda/MainActivity.java
+++ app/src/main/java/com/jairo/agenda/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
 
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        //Mando la información a Analytics
+        Bundle params = new Bundle();
+        params.putString("message","Integración de Firebase completa");
+        mFirebaseAnalytics.logEvent("IniciaHome",params);
     }
 }
--- app/src/main/java/com/jairo/agenda/SplashScreen.java
+++ app/src/main/java/com/jairo/agenda/SplashScreen.java
@@ -6,8 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+import com.google.firebase.analytics.FirebaseAnalytics;
 
+public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +22,6 @@ public class SplashScreen extends AppCompatActivity {
                 finish();
             }
         },2000);
+
     }
 }
