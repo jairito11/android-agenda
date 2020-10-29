cba3f1d9e676e1068ed3806abe653ba82f4b7879
jairo <agust_sani@hotmail.com>
2020-10-29 14:57:44 -0600

    Se hace login

--- app/src/main/java/com/jairo/agenda/HomeActivity.java
+++ app/src/main/java/com/jairo/agenda/HomeActivity.java
@@ -3,11 +3,16 @@ package com.jairo.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+enum ProviderType {
+    BASIC
+}
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
     }
 }
--- app/src/main/java/com/jairo/agenda/MainActivity.java
+++ app/src/main/java/com/jairo/agenda/MainActivity.java
@@ -3,6 +3,8 @@ package com.jairo.agenda;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -54,9 +56,9 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if(task.isSuccessful()){
-
+                                        showHome(etEmail.getText().toString(),ProviderType.BASIC);
                                     }else{
-                                        Toast.makeText(MainActivity.this, "Error en la autenticación", Toast.LENGTH_SHORT).show();
+                                        showAlert();
                                     }
                                 }
                             });
@@ -66,5 +68,50 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        //BotonLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //etEmail.getText().toString().isEmpty()
+                if ((!etEmail.getText().toString().isEmpty() && etEmail.getText().toString().contains("@"))&& !etPassword.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Conectando", Toast.LENGTH_SHORT).show();
+
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(etEmail.getText().toString(),etPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        showHome(etEmail.getText().toString(),ProviderType.BASIC);
+                                    }else{
+                                        showAlert();
+                                    }
+                                }
+                            });
+
+                }else{
+                    Toast.makeText(MainActivity.this, "Utiliza un formato correcto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("Se ha producido un error en la autenticación");
+        builder.setPositiveButton("Aceptar",null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private void showHome(String email, ProviderType provider){
+//        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+//        startActivity(intent);
+//        finish();
+        Intent i = new Intent(this,HomeActivity.class);
+        i.putExtra("email",email);
+        i.putExtra("provider",provider.toString());
+        startActivity(i);
     }
 }
